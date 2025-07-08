0d52fbc04d431b6fa6291754fccc501e8ebe5ab8
kaustubh-CS <123150214+kaustubh-CS@users.noreply.github.com>
2025-07-08 13:38:35 +0530

    git install?

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -x
 
 # --- 0. packages -------------------------------------------------------------
 apt-get update
-apt-get install -y build-essential python3-dev python3-venv wget
+apt-get install -y build-essential python3-dev python3-venv \
+                   wget git ca-certificates
 
 # --- 1. python venv ----------------------------------------------------------
 if [ ! -d ".venv" ]; then
@@ -23,7 +24,8 @@ source ./emsdk/emsdk_env.sh >/dev/null
 
 # --- 3. build ---------------------------------------------------------------
 [ ! -f doomgeneric/doom1.wad ] && \
-  wget https://distro.ibiblio.org/slitaz/sources/packages/d/doom1.wad -O doomgeneric/doom1.wad
+  wget https://distro.ibiblio.org/slitaz/sources/packages/d/doom1.wad \
+       -O doomgeneric/doom1.wad
 
 [ "$1" = clean ] && emmake make -C doomgeneric -f Makefile.pdfjs clean
 emmake make -C doomgeneric -f Makefile.pdfjs -j"$(nproc)"
@@ -31,7 +33,7 @@ emmake make -C doomgeneric -f Makefile.pdfjs -j"$(nproc)"
 mkdir -p out
 cp web/* out/
 python3 embed_file.py file_template.js doomgeneric/doom1.wad out/data.js
-cat pre.js out/data.js doomgeneric/doomgeneric.js > out/compiled.js
+cat pre.js out/data.js doomgeneric/doomgeneric.js       > out/compiled.js
 cat pre.js file_template.js doomgeneric/doomgeneric.js > out/compiled_nowad.js
-python3 generate.py out/compiled.js out/doom.pdf
+python3 generate.py out/compiled.js       out/doom.pdf
 python3 generate.py out/compiled_nowad.js out/doom_nowad.pdf
